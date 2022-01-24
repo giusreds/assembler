033ce493c6ac730b6c585d5496b90f80c9ecb101
Giuseppe Rossi <giuseppe-car@live.it>
2022-01-25 00:46:43 +0100

    Risolto bug percorso con spazi
    Risolto problema per cui spazi nel percorso dalla root alla cartella di lavoro causavano errato funzionamento dei tasks.

--- assembler/files/tasks.sh
+++ assembler/files/tasks.sh
@@ -7,7 +7,7 @@ clear
 if [[ "$1" == "-r" || "$1" == "-d" ]]; then
     # Run or debug
     if [ -f "$2.exe" ]; then
-        cp $2.exe ./$srcname.EXE 1>/dev/null 2>/dev/null
+        cp "$2.exe" ./$srcname.EXE 1>/dev/null 2>/dev/null
         echo "Running $2.exe in DOSBox..."
         if [[ "$1" == "-r" ]]; then
             # Run
@@ -30,13 +30,13 @@ else
     if [[ "$1" == "-a" ]]; then
         # Assemble
         if [ -f "$2.s" ]; then
-            cp $2.s ./$srcname.S 1>/dev/null 2>/dev/null
+            cp "$2.s" ./$srcname.S 1>/dev/null 2>/dev/null
             # Add new line at the end of the file
             echo -en "\n" >> ./$srcname.S 2>/dev/null
             echo "Assembling $2.s in DOSBox..."
             $start_dosbox -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "gcc -o $srcname.S -Wa,-a -Wa,--defsym,DOS=1 >NUL -g $srcname.S" -c "pause" -c "exit" 1>/dev/null 2>/dev/null
             rm -f ./$srcname.S 1>/dev/null 2>/dev/null
-            mv -f ./$srcname.EXE $2.exe 1>/dev/null 2>/dev/null
+            mv -f ./$srcname.EXE "$2.exe" 1>/dev/null 2>/dev/null
             rm -f ./C*.LD ./C*.SWP ./C*.LE ./C*.O ./C*.C 1>/dev/null 2>/dev/null
             clear
         else
