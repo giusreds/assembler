f5d0ecaf2bfcc23a9a9a37b01f3f474fcd50c4c1
Giuseppe Rossi <giuseppe-car@live.it>
2022-01-25 16:14:59 +0100

    Fixed "corrupted executable error" from Mac OS

--- assembler/files/tasks.sh
+++ assembler/files/tasks.sh
@@ -4,20 +4,23 @@ srcname="ASSEMBLY"
 start_dosbox="./files/dosbox.app/Contents/MacOS/DOSBox"
 modded_s=$(echo "$2" | sed 's/\(.*\)\..*/\1/')
 clear
+# Remove MacOS warning message
+xattr -dr com.apple.quarantine ./files/dosbox.app 1>/dev/null 2>&1
+xattr -cr ./files/dosbox.app 1>/dev/null 2>&1
 if [[ "$1" == "-r" || "$1" == "-d" ]]; then
     # Run or debug
     if [ -f "$2.exe" ]; then
-        cp "$2.exe" ./$srcname.EXE 1>/dev/null 2>/dev/null
-        echo "Running $2.exe in DOSBox..."
+        cp "$2.exe" ./$srcname.EXE 1>/dev/null 2>&1
+        echo "Running \"$2.exe\" in DOSBox..."
         if [[ "$1" == "-r" ]]; then
             # Run
-            $start_dosbox -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "$srcname.EXE" -c "echo." -c "pause" -c "exit" 1>/dev/null 2>/dev/null
+            $start_dosbox -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "$srcname.EXE" -c "echo." -c "pause" -c "exit" 1>/dev/null 2>&1
         else
             # Debug
-            $start_dosbox  -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "gdb -x .\files\GAS\gdbstart $srcname.EXE" -c "exit" 1>/dev/null 2>/dev/null
+            $start_dosbox  -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "gdb -x .\files\GAS\gdbstart $srcname.EXE" -c "exit" 1>/dev/null 2>&1
         fi
-        rm -f ./$srcname.EXE 1>/dev/null 2>/dev/null
-        rm -f ./C*.LD ./C*.SWP ./C*.LE ./C*.O ./C*.C 1>/dev/null 2>/dev/null
+        rm -f ./$srcname.EXE 1>/dev/null 2>&1
+        rm -f ./C*.LD ./C*.SWP ./C*.LE ./C*.O ./C*.C 1>/dev/null 2>&1
         clear
     else
         if [ -f "$2.s" ]; then
@@ -30,19 +33,19 @@ else
     if [[ "$1" == "-a" ]]; then
         # Assemble
         if [ -f "$2.s" ]; then
-            cp "$2.s" ./$srcname.S 1>/dev/null 2>/dev/null
+            cp "$2.s" ./$srcname.S 1>/dev/null 2>&1
             # Add new line at the end of the file
-            echo -en "\n" >> ./$srcname.S 2>/dev/null
-            echo "Assembling $2.s in DOSBox..."
-            $start_dosbox -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "gcc -o $srcname.S -Wa,-a -Wa,--defsym,DOS=1 >NUL -g $srcname.S" -c "pause" -c "exit" 1>/dev/null 2>/dev/null
-            rm -f ./$srcname.S 1>/dev/null 2>/dev/null
-            mv -f ./$srcname.EXE "$2.exe" 1>/dev/null 2>/dev/null
-            rm -f ./C*.LD ./C*.SWP ./C*.LE ./C*.O ./C*.C 1>/dev/null 2>/dev/null
+            echo -en "\n" >> ./$srcname.S 2>&1
+            echo "Assembling \"$2.s\" in DOSBox..."
+            $start_dosbox -c "@echo off" -c "MOUNT C ." -c "C:" -c "SET PATH=C:\files\GAS\BIN;%PATH%" -c "SET DJGPP=C:\files\GAS\DJGPP.ENV" -c "cls" -c "gcc -o $srcname.S -Wa,-a -Wa,--defsym,DOS=1 >NUL -g $srcname.S" -c "pause" -c "exit" 1>/dev/null 2>&1
+            rm -f ./$srcname.S 1>/dev/null 2>&1
+            mv -f ./$srcname.EXE "$2.exe" 1>/dev/null 2>&1
+            rm -f ./C*.LD ./C*.SWP ./C*.LE ./C*.O ./C*.C 1>/dev/null 2>&1
             clear
         else
             echo "The current file is not a valid Assembly source file."
         fi
     else
-        echo "Invalid parameter: $1."
+        echo "Invalid parameter: \"$1\"."
     fi
 fi
